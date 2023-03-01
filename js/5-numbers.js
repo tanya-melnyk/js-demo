5e24b97cca10e025943e4273c4a4ad9743bfae91
DenisOrlenko <ghntrdo@gmail.com>
2023-03-01 15:30:03 +0300

    js

--- js/5-numbers.js
+++ js/5-numbers.js
@@ -1,9 +1,15 @@
 // Методи parseInt, parseFloat, isNaN, Number, toFixed
 // Приведення до числа
+console.log('5) Преобразование строки в число: Number()');
+const userAgeStr = prompt('Enter your age:');
+const userAgeNum = Number(userAgeStr);
 
-// const userAgeStr = prompt('Enter your age:');
+console.log('typeof -', typeof userAgeStr);
+console.log(userAgeStr);
 
-// const userAgeNum = Number(userAgeStr);
+console.log('typeof -', typeof userAgeNum);
+console.log('Number(userAgeStr) - подсветка значения голубым цветом -', userAgeNum);
+console.log('')
 // // const userAgeNum = parseInt(userAgeStr);
 // // const userAgeNum = parseFloat(userAgeStr);
 // // const userAgeNum = +userAgeStr;
@@ -15,7 +21,22 @@
 // console.log(userAgeNum);
 
 ///////// parseInt, parseFloat
+let width = '500px'
+const widthToNum = parseInt(width);
+console.log('6) parseInt(width) - распарсиваю строку "500px" в целое число -', widthToNum);
 
+let height = 'px125px';
+const invalidString = parseInt(height);
+console.log('6.1) parseInt("px125px") - в строке значение начинается не с цифры - невалидная число, Not a Number - "px125px" -', invalidString)
+
+let letterSpacing = '0.99em'
+const letterSpacingToNum = parseFloat(letterSpacing);
+console.log('6.2) parseFloat("0.99em") - распарсивание строки в плавающее число(т.е. десятые,сотые, тысячные..) - "0.99em" -', letterSpacingToNum)
+
+let font = '125,555'
+const invalidString2 = parseFloat(font);
+console.log('6) parseFloat("125,555") - ЗАПЯТАЯ - невалидный символ для числа, поэтому обрывается считывание - Not a Number - "125,555" -', invalidString2)
+console.log('')
 // let font = '25.578964rem';
 
 // const integer = parseInt(font);
@@ -35,8 +56,31 @@
 
 //////////////// toFixed
 
-// const str = '5.9486394474958799';
-// const number = parseFloat(str);
+const str = '5.9486394474958799px';
+const numberToFloat = parseFloat(str)
+console.log('7) Распарсиваю рядок методом parseFloat("5.9486394474958799px")', numberToFloat)
+
+let numberToFixed = numberToFloat.toFixed(4)
+console.log('7.1) .toFixed(4) - округление числа с указанием кол-ва чисел после запятой:', numberToFixed)
+
+const numberToFixedToNumber = Number(numberToFixed)
+console.log('7.2) Метод .toFixed(4) преобразовует число в рядок, поэтому, чтобы заново преоброзовать рядок в число применяю Конструктор Number():', numberToFixedToNumber)
+
+const string = '5.9486';
+const numberToFloat2 = parseFloat(string)
+numberToFixed = numberToFloat2.toFixed()
+console.log('7.3) Метод .toFixed() если в скобках не указано значение, то округляется до целого числа "5.9486" =>', numberToFixed)
+
+// Цепочка методов =>chaining
+const roundedNumber = Number(parseFloat(str).toFixed(1));
+console.log('7.4) Цепочка методов => chaining - сокращенная запись - Number(parseFloat(str).toFixed(1)):', roundedNumber)
+
+///////// isNaN
+const str3 = '555px';
+
+const numberFromString = Number(str);
+console.log(numberFromString); // NaN
+console.log('Метод .isNaN("555px") проверяет валидная строка или невалидная - true - значит невалидная т.е. в строке есть посторонний символы - это значит, что надо распарсить строку(parseInt, parseFloat) перед тем как приводить к числу (Number())', Number.isNaN(numberFromString));
 
 // console.log(number);
 
