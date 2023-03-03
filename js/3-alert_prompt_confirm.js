2e9fea52a4f3184b982c0a8f9ec743887626ae48
DenisOrlenko <ghntrdo@gmail.com>
2023-03-03 16:09:01 +0300

    js

--- js/3-alert_prompt_confirm.js
+++ js/3-alert_prompt_confirm.js
@@ -1,29 +1,32 @@
+// Порядок срабатывания скриптов
+const number = 10
+console.log('3 - срабатывает скрипт с атрибутом defer в <head> -', number)
+console.log('')
+//
 // 1) Вивести на екран повідомлення 'Welcome to my page!'
 
-// alert('Welcome to my page!');
+alert('Уведоление - alert()');
 
 // 2) Попросити користувача ввести свій вік 'How old are you?' і вивести в консоль те, що він ввів (перевірити тип даних)
 
-// const userAge = prompt('How old are you?');
+const userAge = prompt('Обращение к пользователю, который должен ввести данные в поле - возвращает в консоль введденные данные - prompt()');
 
-// console.log(userAge);
+console.log(userAge);
 
 // console.log('userAge =', userAge);
 // console.log('typeof userAge =', typeof userAge);
 
 // 3) Запитати у користувача, чи він є студентом 'Are you a student?' і вивести в консоль те, що він відповів
 
-// const isStudent = confirm('Are you a student?');
+const isStudent = confirm('Обращение к пользователю, который должен ответить ок/cansel(возвращает в консоль соответсвенно true/false) - confirm()');
 
-// console.log(isStudent);
+console.log(isStudent);
 
 
 
 //
 //
-// Порядок срабатывания скриптов
-const number = 10
-console.log('3 - срабатывает скрипт с атрибутом defer в <head> -', number)
+
 //
 // alert("hello")
 // window.alert("hi")
